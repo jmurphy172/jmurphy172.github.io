679053fa16baf0d65f6885ae7c8f6ff8b06ceaa5
James <james.murphy@wrighthealthgroup.com>
2023-10-17 09:25:36 +0100

    js for colour changer

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ changeThisPageFunc = function() {
     document.body.style.backgroundColor = "lightblue";
     document.body.innerHTML = "<h1>I am a little blue today</h1>";
 }
+
+
+changeColor = function() {
+           red = document.querySelector('#redi').value;
+           green = document.querySelector('#greeni').value;
+           blue = document.querySelector('#bluei').value;
+           colorStr = "rgb("+red+","+green+","+blue+")";
+           document.body.style.backgroundColor = colorStr;
+       }
